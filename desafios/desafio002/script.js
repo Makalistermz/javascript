68256ad486d6c633f9beb50bc3873b3c4f977b71
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-01-10 21:37:12 -0300

    .

---
+++ desafios/desafio002/script.js
@@ -0,0 +1,5 @@
+function desafio() {
+    const nome = window.prompt('Qual seu nome?')
+    const idade = prompt('quantos anos voce tem?')
+    alert(`Acabei de conhecer o ${nome} que tem ${idade} anos!`)
+}
