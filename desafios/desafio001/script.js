88a713228c8e47801b683f481ab5d1823cd059b8
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-01-10 21:38:07 -0300

    .
    .

---
+++ desafios/desafio001/script.js
@@ -0,0 +1,11 @@
+function um() {
+    alert('Você clicou no botão 1!');
+}
+
+function dois() {
+    alert('Você clicou no segundo botão!')
+}
+
+function tres() {
+    alert('Você clicou no terceiro botão!')
+}
