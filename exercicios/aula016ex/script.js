a3bd5f1e50418e01a632f33b937d97de5ceab001
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-02-02 10:34:12 -0300

    Update script.js

--- exercicios/aula016ex/script.js
+++ exercicios/aula016ex/script.js
@@ -1,13 +1,16 @@
+let valores = [];
+
 function adicionar() {
-    let num = document.getElementById('numero');
-    let tabu = document.getElementById('tabuada');
+        let num = document.getElementById('numero');
+        let tabu = document.getElementById('tabuada');
 
-    let nu = Number(num.value);
-
+        let nu = Number(num.value);
+        
     if (nu <= 100 && nu >= 0) {
         let item = document.createElement('option');
         item.text = `Valor ${nu} adicionado!`;
         tabu.appendChild(item);
+        valores.push(nu)
     } else {
         window.alert('Valor Não definido ou Já existe este valor');
     }
@@ -15,12 +18,20 @@ function adicionar() {
 }
 
 function finalizar() {
-    let num = document.getElementById('numero');
-    let tabu = document.getElementById('tabuada');
     let res = document.getElementById('res');
+    let tabu = document.getElementById('tabuada');
 
-    let nu = Number(num.value);
+    if (valores.length === 0) {
+        window.alert('Adicione valores antes!');
+        return;
+    }
 
-    res.innerHTML = `Ao todo temos ${tabu.length} números cadastrados.`;
-    res.innerHTML = `O maior valor cadastrado foi ${tabu.length.}.`;
+    let maior = Math.max(...valores)
+    let menor = Math.min(...valores)
+
+    res.innerHTML = `
+    Ao todo temos ${tabu.length} números cadastrados.<br>
+    O maior valor cadastrado foi ${maior}. <br>
+    O menor valor cadastrado foi ${menor}. <br>
+    `;
 }
