358271048bd56e6a60244f5b0e22736d2a20c582
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-04-07 21:12:56 -0300

    .

--- exercicios/barraprogressiva/script.js
+++ exercicios/barraprogressiva/script.js
@@ -1,9 +1,11 @@
 let containerbarra = document.querySelector('.container-ativa');
+const btn = document.querySelector('.btn span');
 
 function ativar() {
     const barra = document.getElementById('barra');
     barra.classList.add('ativa');
 
     barra.addEventListener('animationend', function() {
+        btn.textContent = 'Concluído!';
     })
 }
