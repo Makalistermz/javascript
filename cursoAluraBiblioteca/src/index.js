05d56bcfd349ef87dbdafb4ab69bb5ce0c168888
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-07-01 19:51:22 -0300

    Update index.js

--- cursoAluraBiblioteca/src/index.js
+++ cursoAluraBiblioteca/src/index.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-console.log('O caminho do arquivo é:', link); // Adicione esta linha
-
 fs.readFile (link, 'utf-8', (erro, texto) => {
     console.log(texto);
 });
